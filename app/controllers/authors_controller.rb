568c1b2d26fdfb79935c8046fae19f5b7b78cfa4
Michael Roose <mjroose@gmail.com>
2018-11-17 20:46:55 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -18,7 +18,12 @@ class AuthorsController < ApplicationController
   end
 
   def update
-
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
